2ac28b9fb8a51d0ba7356e5e8e05422a8be1b49b
NewGuyHere <jvele019@fiu.edu>
2014-08-17 20:02:17 -0400

    initial project version

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Man
+{
+    int age;
+    char name[20];
+    char job [20];
+}worker, father;
+
+int main()
+{
+    //worker1;
+   strcpy(worker.name, "jon");
+   strcpy(father.name, "jack");
+
+ // strcpy(Man.name, "herb");
+    printf("you are %s\n",worker.name);
+      printf("you are %s\n",father.name);
+   // printf("you are %s\n",Man.name);
+    return 1;
+}
